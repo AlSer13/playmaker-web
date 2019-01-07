946e62953b97edf0e74461990a8e1f30faf0328a
Uchvatov Semyon <s-uchvatov@yandex.ru>
2019-01-07 19:43:45 +0300

    fix avatar on user-settings page
    Signed-off-by: Semyon Uchvatov <s-uchvatov@yandex.ru>

--- src/app/user/user-settings/user-settings.component.ts
+++ src/app/user/user-settings/user-settings.component.ts
@@ -23,7 +23,7 @@ export class UserSettingsComponent implements OnInit {
 
     async ngOnInit() {
         try {
-            this.user = await this.userService.getUserInfo(this.authService.user.username);
+            this.user = new User(await this.userService.getUserInfo(this.authService.user.username));
         } catch (error) {
             this.handleError(error);
         }
