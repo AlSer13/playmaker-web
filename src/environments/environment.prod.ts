be5775643337af8c9160b37bf3fe45b443ffca9a
Uchvatov Semyon <s-uchvatov@yandex.ru>
2019-01-23 01:40:25 +0300

    fix currency sign
    Signed-off-by: Semyon Uchvatov <s-uchvatov@yandex.ru>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: true,
-    localURL: 'http://localhost:3000/api'
+    localURL: 'http://localhost:3000/api',
+    avatarURL: 'http://localhost:3000/api/user/avatar/',
+    steamMediaURL: 'http://media.steampowered.com/apps/dota2/images/',
 };
