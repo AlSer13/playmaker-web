ddfd792df3bd631930b2d8447c849d740e351b29
AlSer13 <36701344+alser13@users.noreply.github.com>
2019-01-23 00:02:53 +0300

    Feedback form without api call

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {LocalUserService} from '../../services/local-user.service';
 
 @Component({
-  selector: 'app-help',
-  templateUrl: './help.component.html',
-  styleUrls: ['./help.component.css']
+    selector: 'app-help',
+    templateUrl: './help.component.html',
+    styleUrls: ['./help.component.css']
 })
 export class HelpComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    constructor(private userService: LocalUserService) {
+    }
+
+    feedbackForm = new FormGroup({
+        email: new FormControl(this.userService.getUser() && this.userService.getUser().email ?
+            this.userService.getUser().email : '', [Validators.email, Validators.maxLength(100)]),
+        message: new FormControl('', Validators.maxLength(2000))
+    });
+
+    ngOnInit() {
+    }
+
+    sendMessage() {
+        console.log(this.feedbackForm.get('email').value, this.feedbackForm.get('message').value);
+    }
 
 }
